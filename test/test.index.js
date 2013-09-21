8b426728631a8c7c7680bbac2dc9994970eabaaa
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-21 17:27:58 +0200

    Added test.index.js

---
+++ test/test.index.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var program = require('commander');
+var CommanderUtils = require('./../lib/index');
+var pkg = require('./../package.json');
+var programUtils = new CommanderUtils(program, pkg);
+
+
+describe('lib/index.js', function() {
+  
+  describe('#version()', function() {
+    it('set the version of the commander.js tool.', function() {
+      assert.equal( pkg.version, programUtils.version() );
+    })
+  })
+
+})
