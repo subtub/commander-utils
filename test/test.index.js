0a6b51497230a2fe95f307b32492c4c878213058
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-22 00:10:52 +0200

    WIP at commandUpdate

--- test/test.index.js
+++ test/test.index.js
@@ -13,22 +13,4 @@ describe('lib/index.js', function() {
     })
   })
 
-  describe('#getPackageJsonFromUrl()', function() {
-    it('should return the package.json object.', function() {
-      programUtils.getPackageJsonFromUrl('https://github.com/subtub/commander-utils/raw/master/test/files/package.json', function(data) {
-        var result = {
-          name: 'test',
-          version: '1.0.0'
-        };
-        assert.equal( result, data );
-      })
-    })
-    
-    it('should return false if the url is not correct.', function() {
-      programUtils.getPackageJsonFromUrl('not/correct/url', function(data) {
-        assert.equal( false, data );
-      })
-    })
-  })
-
 })
