8a863b8f135925f4214a6d4f58bf6777901a22f3
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-21 19:20:18 +0200

    Added getPackageJsonFromUrl function

--- test/test.index.js
+++ test/test.index.js
@@ -8,8 +8,26 @@ var programUtils = new CommanderUtils(program, pkg);
 describe('lib/index.js', function() {
   
   describe('#version()', function() {
-    it('set the version of the commander.js tool.', function() {
+    it('should return the version of the commander.js tool.', function() {
       assert.equal( pkg.version, programUtils.version() );
+    })
+  })
+
+  describe('#getPackageJsonFromUrl()', function() {
+    it('should return the package.json object.', function() {
+      programUtils.getPackageJsonFromUrl('https://github.com/subtub/commander-utils/raw/master/test/files/package.json', function(data) {
+        var result = {
+          name: 'test',
+          version: '1.0.0'
+        };
+        assert.equal( result, data );
+      })
+    })
+    
+    it('should return false if the url is not correct.', function() {
+      programUtils.getPackageJsonFromUrl('not/correct/url', function(data) {
+        assert.equal( false, data );
+      })
     })
   })
 
