124f5c57703d19d2d42a9e199df63bcbecdad2e9
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-23 11:33:13 +0200

    Added log functions and tests

--- test/test.index.js
+++ test/test.index.js
@@ -13,4 +13,32 @@ describe('lib/index.js', function() {
     })
   })
 
+  describe('#log()', function() {
+    describe('#setSilent()', function() {
+      it('should return true.', function() {
+        programUtils.log.setSilent(true);
+        assert.equal( true, programUtils.log.config.silent );
+      })
+      it('should return false.', function() {
+        programUtils.log.setSilent(false);
+        assert.equal( false, programUtils.log.config.silent );
+      })
+    })
+    describe('#setColorless()', function() {
+      it('should return true.', function() {
+        programUtils.log.setColorless(true);
+        assert.equal( true, programUtils.log.config.colorless );
+      })
+      it('should return false.', function() {
+        programUtils.log.setColorless(false);
+        assert.equal( false, programUtils.log.config.colorless );
+      })
+    })
+    describe('#info()', function() {
+      it('should return the log message.', function() {
+        assert.equal( 'hello world', programUtils.log.info('hello world') );
+      })
+    })
+  })
+
 })
