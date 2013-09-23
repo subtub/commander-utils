739b29c3b52aae68f3d1b23a9d3a9b9607bbf774
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-09-23 13:46:04 +0200

    Added example and test

---
+++ test/test.example.sample.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var shell = require('shelljs/global');
+
+
+describe('example/sample', function() {
+  
+  var path = 'node '+process.cwd()+'/example/sample ';
+
+  it('execute the sample cli without command or option.', function() {
+  	exec(path);
+  })
+  it('execute the sample cli with --help option.', function() {
+  	exec(path+'--help');
+  })
+  it('execute the sample cli with --version option.', function() {
+  	exec(path+'--version');
+  })
+  it('execute the sample cli with --subtub option.', function() {
+  	exec(path+'--subtub');
+  })
+  it('execute the sample cli with --subtub and --silent option.', function() {
+  	exec(path+'--subtub --silent');
+  })
+  it('execute the sample cli with --subtub and --colorless option.', function() {
+  	exec(path+'--subtub --colorless');
+  })
+
+})
